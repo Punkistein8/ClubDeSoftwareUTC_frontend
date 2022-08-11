c63c1a9c00297e25040175e9cc0fd5d80dbe8378
Punkistein8 <henry.guerrero0229@utc.edu.ec>
2022-08-11 09:40:47 -0500

    prueba de funete comfortaa

--- src/layout/Landing/components/Hero.jsx
+++ src/layout/Landing/components/Hero.jsx
@@ -3,6 +3,8 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import { Link } from 'react-router-dom'
+
 import mainMain from '../../../assets/imagenAbout.jpg'
 import mainClub from '../../../assets/main-club.png'
 import mainSaul from '../../../assets/main-saul.png'
@@ -60,12 +62,11 @@ export default function Example() {
                       </Popover.Button>
                     </div>
                   </div>
-                  <a
-                    href="#"
+                  <p
                     className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
                   >
                     Log in
-                  </a>
+                  </p>
                 </div>
               </Popover.Panel>
             </Transition>
@@ -74,29 +75,22 @@ export default function Example() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Nosotros somos el</span>{' '}
-                <span className="block text-indigo-600 xl:inline" style={{ fontFamily: 'Comfortaa' }}>Club de Software</span>
+                <span className="block text-center xl:inline">Nosotros somos el</span>{' '}
+                <span className="block text-center text-indigo-600 xl:inline">Club de Software</span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-justify">
+                Tenemos una clara visión de que el altruismo intelectual y desinteresado formará una
+                herramienta de transformación que nos permitirá crear una sociedad más justa, capaz y equitativa.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                  >
-                    Get started
-                  </a>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                  >
-                    Live demo
-                  </a>
+                  <Link to="login">
+                    <p
+                      className="mt-10 w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                    >
+                      Prueba la versión beta de la aplicación
+                    </p>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -110,6 +104,6 @@ export default function Example() {
           alt=""
         />
       </div>
-    </div>
+    </div >
   )
 }
--- src/layout/Landing/components/NavBar.jsx
+++ src/layout/Landing/components/NavBar.jsx
@@ -4,7 +4,9 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 import logoClub from '../../../assets/logoBlanco.svg'
+import logoClubAzul from '../../../assets/logoClub.png'
 import logoClubLetras from '../../../assets/logoBlancoLetras.svg'
+import { Link } from 'react-router-dom'
 
 const navigation = [
   { name: 'Inicio', href: '#', current: true },
@@ -82,7 +84,7 @@ export default function Example() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={logoClub}
                         alt=""
                       />
                     </Menu.Button>
@@ -96,15 +98,16 @@ export default function Example() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Plataforma
-                          </a>
+                          <Link to={'utcstudent'}>
+                            <p
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            >
+                              Plataforma
+                            </p>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
